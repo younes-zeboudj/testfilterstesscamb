4c5c048f0f6050365551ec9b08be86f701b53023
younes-zeboudj <younes.zeboudj@gmail.com>
2023-08-30 12:54:01 +0200

    u

--- child.js
+++ child.js
@@ -219,7 +219,7 @@ async function testFilter(filter) {
 
             await new Promise(resolve => {
                 try {
-                    const base64= fs.readFileSync(file).toString('base64')
+                    const base64= `data:image/jpeg;base64,`+fs.readFileSync(file).toString('base64')
                     Caman(base64, function () {
                         console.log(`read file ${file} ${'...'}`);
                         for (const f of filters) {
